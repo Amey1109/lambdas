37afc40c29fddc0d8fd419a88da02c255ef3dcf4
Amey Bhogaonkar <bhogaonkaramey@gmail.com>
2026-01-16 12:29:16 +0530

    Test: Params

--- lambdas/simple-js/index.js
+++ lambdas/simple-js/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello from plain JavaScript Lambda",
+      message: "Hello from plain JavaScript Lambda Dev",
       input: event
     })
   };
